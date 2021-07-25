0c73afa8f61ff664f1d63125b5333fff776dda5e
Denny <dennynice@icloud.com>
2021-07-25 23:50:09 +0200

    my commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict"; //для использования совремнного стандарта javascript se6
+
+const category = 'toys';
+
+console.log( `https://someurl.com/${category}/5`);
+
+const user ="Ivan";
+
+alert(`Hello, ${user}`);
